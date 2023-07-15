ec16082e79347b353f4948b77f64d6c09362992a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-15 21:12:13 +0600

    add a form for add new book

--- src/components/AddNewBookForm.tsx
+++ src/components/AddNewBookForm.tsx
@@ -1,100 +1,118 @@
+import { useForm } from 'react-hook-form';
+
 import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { useLoginMutation } from '@/redux/features/user/userApi';
-import { loginUser } from '@/redux/features/user/userSlice';
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import { FcGoogle } from 'react-icons/fc';
-import { useLocation, useNavigate } from 'react-router-dom';
 
-interface LoginFormInputs {
-  email: string;
-  password: string;
+interface IAddNewBookInputs {
+  title: string;
+  author: string;
+  genre: string;
+  publicationDate: string;
 }
 const AddNewBookForm = () => {
   const {
     register,
     handleSubmit,
+    clearErrors,
+    reset,
     formState: { errors },
-  } = useForm<LoginFormInputs>();
+  } = useForm<IAddNewBookInputs>();
 
-  const { user, isLoading } = useAppSelector((state) => state.user);
-  const [loginUserUsingDb] = useLoginMutation();
-  const dispatch = useAppDispatch();
+  const currentTime = new Date();
 
-  const { state } = useLocation();
-
-  const path = state?.path;
-
-  const navigate = useNavigate();
-
-  const onSubmit = async (data: LoginFormInputs) => {
-    console.log(data);
-    const { email, password } = data;
-
-    await loginUserUsingDb({ email, password });
-
-    dispatch(loginUser({ email: data.email, password: data.password }));
+  const onSubmit = async (data: IAddNewBookInputs) => {
+    console.log({ ...data, publicationDate: currentTime });
   };
 
-  useEffect(() => {
-    if (user.email && !isLoading) {
-      path ? navigate(path) : navigate('/');
-    }
-  }, [user.email, isLoading, navigate, path]);
+  const clearForm = () => {
+    clearErrors();
+    reset();
+  };
 
   return (
-    <div className="grid grid-cols-1">
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Add A New Book</CardTitle>
+      </CardHeader>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              {...register('email', { required: 'Email is required' })}
-            />
-            {errors.email && <p>{errors.email.message}</p>}
-            <Input
-              id="password"
-              placeholder="your password"
-              type="password"
-              autoCapitalize="none"
-              autoComplete="password"
-              {...register('password', { required: 'Password is required' })}
-            />
-            {errors.password && <p>{errors.password.message}</p>}
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="title">Title*</Label>
+              <Input
+                id="title"
+                {...register('title', {
+                  required: 'Book title is required',
+                })}
+                type="text"
+              />
+              <p className="text-red-500">{errors.title?.message}</p>
+            </div>
           </div>
-          <Button>Login with email</Button>
-        </div>
+        </CardContent>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="author">Author*</Label>
+              <Input
+                id="author"
+                {...register('author', {
+                  required: 'Author name is required',
+                })}
+                type="text"
+              />
+              <p className="text-red-500">{errors.author?.message}</p>
+            </div>
+          </div>
+        </CardContent>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="genre">Genre*</Label>
+              <Input
+                id="genre"
+                {...register('genre', {
+                  required: 'Genre is required',
+                })}
+                type="text"
+              />
+              <p className="text-red-500">{errors.genre?.message}</p>
+            </div>
+          </div>
+        </CardContent>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="publicationDate">Publication Date*</Label>
+              <Input
+                id="publicationDate"
+                {...register('publicationDate', {
+                  required: 'Publication Date is required',
+                })}
+                defaultValue={currentTime.toUTCString()}
+                disabled={true}
+                type="text"
+              />
+              <p className="text-red-500">{errors.publicationDate?.message}</p>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button onClick={() => clearForm()} type="button">
+            Clear
+          </Button>
+          <Button type={'submit'}>Add Book</Button>
+        </CardFooter>
       </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <Button
-        variant="outline"
-        type="button"
-        className="flex items-center justify-between"
-      >
-        <p>Google</p>
-        <FcGoogle />
-      </Button>
-    </div>
+    </Card>
   );
 };
 
