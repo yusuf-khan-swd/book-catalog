d0eee32f1037dd15d34dd7abf06cc327c6de16df
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-16 12:22:59 +0600

    refactor product to book

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,19 +1,19 @@
 import { addToCart } from '@/redux/features/cart/cartSlice';
 import { useAppDispatch } from '@/redux/hooks';
-import { IProduct } from '@/types/globalTypes';
+import { IBook } from '@/types/globalTypes';
 import { Link } from 'react-router-dom';
 import { Button } from './ui/button';
 import { toast } from './ui/use-toast';
 
 interface IProps {
-  product: IProduct;
+  book: IBook;
 }
 
-export default function BookCard({ product }: IProps) {
+export default function BookCard({ book }: IProps) {
   const dispatch = useAppDispatch();
 
-  const handleAddProduct = (product: IProduct) => {
-    dispatch(addToCart(product));
+  const handleAddProduct = (book: IBook) => {
+    dispatch(addToCart(book));
     toast({
       description: 'Product Added',
     });
@@ -21,16 +21,16 @@ export default function BookCard({ product }: IProps) {
   return (
     <div>
       <div className="rounded-2xl h-[480px] flex flex-col items-start justify-between p-5 overflow-hidden shadow-md border border-gray-100 hover:shadow-2xl hover:scale-[102%] transition-all gap-2">
-        <Link to={`/book-details/${product._id}`} className="w-full">
-          <img src={product?.image} alt="product" />
-          <h1 className="text-xl font-semibold">{product?.name}</h1>
+        <Link to={`/book-details/${book._id}`} className="w-full">
+          <img src={book?.image} alt="product" />
+          <h1 className="text-xl font-semibold">{book?.name}</h1>
         </Link>
-        <p>Rating: {product?.rating}</p>
+        <p>Rating: {book?.rating}</p>
         <p className="text-sm">
-          Availability: {product?.status ? 'In stock' : 'Out of stock'}
+          Availability: {book?.status ? 'In stock' : 'Out of stock'}
         </p>
-        <p className="text-sm">Price: {product?.price}</p>
-        <Button variant="default" onClick={() => handleAddProduct(product)}>
+        <p className="text-sm">Price: {book?.price}</p>
+        <Button variant="default" onClick={() => handleAddProduct(book)}>
           Add to cart
         </Button>
       </div>
