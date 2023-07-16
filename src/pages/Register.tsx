d4ffc4611834b1f97dca498b82face0d848178e0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-16 15:18:49 +0600

    remove left side image from login and register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,6 @@
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { Link } from 'react-router-dom';
-import logo from '../assets/images/technet-logo-white.png';
 import { RegisterForm } from '../components/RegisterForm';
 
 export default function Register() {
@@ -17,23 +16,7 @@ export default function Register() {
         >
           Login
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div
-            className="absolute inset-0 bg-cover"
-            style={{
-              backgroundImage:
-                'url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)',
-            }}
-          />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <Link to="/">
-              <img className="h-8" src={logo} alt="" />
-            </Link>
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2"></blockquote>
-          </div>
-        </div>
+
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
