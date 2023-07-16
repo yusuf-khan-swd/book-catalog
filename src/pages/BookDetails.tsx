f635a4bf2c02b109144d966c599f963b6bff4265
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-16 13:38:56 +0600

    get book details

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,32 +1,29 @@
-import BookReview from '@/components/BookReview';
 import { Button } from '@/components/ui/button';
 import { useSingleBookQuery } from '@/redux/features/books/bookApi';
 import { useParams } from 'react-router-dom';
 
 export default function BookDetails() {
   const { id } = useParams();
-
   const { data: book, isLoading } = useSingleBookQuery(id);
-  console.log(book);
 
   if (isLoading) {
     return <h1 className="text-center">Loading...</h1>;
   }
 
+  const date = `${book.publicationDate}`;
+
   return (
     <>
-      <div className="flex max-w-7xl mx-auto items-center border-b border-gray-300">
-        <div className="w-[50%]">
-          <img src={book?.image} alt="" />
-        </div>
-        <div className="w-[50%] space-y-3">
-          <h1 className="text-3xl font-semibold">{book?.name}</h1>
-          <p className="text-xl">Rating: {book?.rating}</p>
-          <ul className="space-y-1 text-lg"></ul>
+      <div className="flex min-h-[290px] max-w-7xl mx-auto items-center border-b border-gray-300">
+        <div className="space-y-3">
+          <h1 className="text-3xl font-semibold">{book?.title}</h1>
+          <p className="text-xl">Rating: {book?.author}</p>
+          <p>Genre: {book?.genre}</p>
+          <p>Publication Date: {date}</p>
           <Button>Add to Bookmark</Button>
         </div>
       </div>
-      <BookReview id={id!} />
+      {/* <BookReview id={id!} /> */}
     </>
   );
 }
