5ef4463e9365068b3419dfde4af6abfb0c955a60
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-16 13:25:59 +0600

    show recently added book to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,24 @@
+import BookCard from '@/components/BookCard';
+import { useGetRecentBooksQuery } from '@/redux/features/books/bookApi';
+import { IBook } from '@/types/globalTypes';
+import { Key } from 'react';
+
 export default function Home() {
-  return <div className=""></div>;
+  const { data, isLoading } = useGetRecentBooksQuery(undefined);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  const recentBooksData = data?.data;
+
+  return (
+    <div className="grid max-w-7xl mx-auto relative ">
+      <div className="grid grid-cols-3 gap-10 pb-20">
+        {recentBooksData?.map((book: IBook, index: Key | null | undefined) => (
+          <BookCard key={index} book={book} />
+        ))}
+      </div>
+    </div>
+  );
 }
