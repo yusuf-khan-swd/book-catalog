9a76b3b1e2a8fb9287ee598e2a47251b58a0eb0c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-17 19:09:57 +0600

    add wishlist reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from './api/apiSlice';
 import userReducer from './features/user/userSlice';
+import wishlistReducer from './features/wishlist/wishlistSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    wishlist: wishlistReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleWare) =>
