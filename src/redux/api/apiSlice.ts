5d6b42ec51b88cb2f887334cf435179f0b1009dc
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-17 12:30:17 +0600

    added vercel deployed backend base url

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }), //todo: for local=> http://localhost:5000
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://book-catalog-server-ashy.vercel.app',
+  }),
   tagTypes: ['reviews'],
   endpoints: () => ({}),
 });
+
+// http://localhost:5000
+// https://book-catalog-server-ashy.vercel.app
