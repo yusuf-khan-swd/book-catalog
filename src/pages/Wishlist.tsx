232a1b74c47d509b879b1bcaf1b6f31668b55221
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-17 18:56:16 +0600

    add wishlist page

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,5 +1,31 @@
+import WishlistCard from '@/components/WishlistCard';
+import { useGetWishlistQuery } from '@/redux/features/books/bookApi';
+import { useAppSelector } from '@/redux/hooks';
+import { IWishlist } from '@/types/globalTypes';
+import { Key } from 'react';
+
 const Wishlist = () => {
-  return <div></div>;
+  const { user, isLoading: userLoading } = useAppSelector(
+    (state) => state.user
+  );
+
+  const { data: wishlistData, isLoading } = useGetWishlistQuery(user.email, {
+    refetchOnMountOrArgChange: true,
+  });
+
+  return (
+    <div className="min-h-screen">
+      <div className="grid max-w-7xl mx-auto relative ">
+        <div className="grid grid-cols-3 gap-10 pb-20">
+          {wishlistData?.map(
+            (wishlist: IWishlist, index: Key | null | undefined) => (
+              <WishlistCard key={index} book={wishlist.book} />
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Wishlist;
