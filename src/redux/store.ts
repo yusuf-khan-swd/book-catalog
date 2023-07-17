a3bd5e4778e64105a2d988cc167825c6c8831cfe
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-17 16:36:21 +0600

    remove bookSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from './api/apiSlice';
-import bookReducer from './features/books/bookSlice';
 import userReducer from './features/user/userSlice';
 
 const store = configureStore({
   reducer: {
-    book: bookReducer,
     user: userReducer,
     [api.reducerPath]: api.reducer,
   },
