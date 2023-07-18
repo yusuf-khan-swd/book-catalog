d0de4997896d6d9f638405cc4ae21a46addfb3d3
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-18 19:27:48 +0600

    [done] filter by reading now plan to read finished

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,10 +1,14 @@
 import WishlistCard from '@/components/WishlistCard';
+import { Button } from '@/components/ui/button';
 import { useGetWishlistQuery } from '@/redux/features/wishlist/wishlistApi';
 import { useAppSelector } from '@/redux/hooks';
 import { IWishlist } from '@/types/globalTypes';
-import { Key } from 'react';
+import { Key, useState } from 'react';
 
 const Wishlist = () => {
+  const [filterWishlistData, setFilterWishlistData] = useState([]);
+  const [isFilterData, setIsFilterData] = useState('');
+
   const { user, isLoading: isUserLoading } = useAppSelector(
     (state) => state.user
   );
@@ -18,18 +22,73 @@ const Wishlist = () => {
     return <p className="min-h-screen text-center">Loading...</p>;
   }
 
+  const handleAllWishlist = () => {
+    setFilterWishlistData(wishlistData);
+    setIsFilterData('All');
+  };
+
+  const handleReadingNow = () => {
+    const result = wishlistData.filter(
+      (data: IWishlist) => data.currentlyReading === true
+    );
+    setFilterWishlistData(result);
+    setIsFilterData('Reading Now');
+  };
+  const handlePlanToRead = () => {
+    const result = wishlistData.filter(
+      (data: IWishlist) => data.planToRead === true
+    );
+    setFilterWishlistData(result);
+    setIsFilterData('Plan to Read');
+  };
+  const handleFinished = () => {
+    const result = wishlistData.filter(
+      (data: IWishlist) => data.finished === true
+    );
+    setFilterWishlistData(result);
+    setIsFilterData('Finished');
+  };
+
   return (
     <div className="min-h-screen">
-      {wishlistData.length < 1 && <p>No book added in wishlist</p>}
-      <div className="grid max-w-7xl mx-auto relative ">
-        <div className="grid grid-cols-3 gap-10 pb-20">
-          {wishlistData?.map(
-            (wishlist: IWishlist, index: Key | null | undefined) => (
-              <WishlistCard key={index} wishlist={wishlist} />
-            )
+      {wishlistData.length === 0 && (
+        <p className="mx-2 my-3">No book added in wishlist</p>
+      )}
+      {wishlistData.length > 0 && (
+        <div className="grid max-w-7xl mx-auto relative ">
+          <div className="flex mb-3">
+            <Button variant="link" onClick={handleAllWishlist}>
+              All
+            </Button>
+            <Button variant="link" onClick={handleReadingNow}>
+              Reading Now
+            </Button>
+            <Button variant="link" onClick={handlePlanToRead}>
+              Plan to read
+            </Button>
+            <Button variant="link" onClick={handleFinished}>
+              Finished
+            </Button>
+          </div>
+          {filterWishlistData.length === 0 && isFilterData && (
+            <p className="mx-2 my-3">No book added in {isFilterData}</p>
           )}
+          <div className="grid grid-cols-3 gap-10 pb-20">
+            {filterWishlistData.length > 0 &&
+              filterWishlistData?.map(
+                (wishlist: IWishlist, index: Key | null | undefined) => (
+                  <WishlistCard key={index} wishlist={wishlist} />
+                )
+              )}
+            {!isFilterData &&
+              wishlistData?.map(
+                (wishlist: IWishlist, index: Key | null | undefined) => (
+                  <WishlistCard key={index} wishlist={wishlist} />
+                )
+              )}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
